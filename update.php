22f3bfb65c1c71c121dc02670dc4d1993953095d
Percy Yeung <percy@codemettle.com>
2015-09-26 17:47:25 -0400

    First real push
    not complete.

--- update.php
+++ update.php
@@ -5,5 +5,54 @@ file_put_contents('logger.txt', 'POST: '.print_r($_POST, true).'GET: '.print_r($
 file_put_contents('logger.txt', 'BODY: '.print_r(json_decode(file_get_contents('php://input')), true), FILE_APPEND);
 file_put_contents('logger.txt', "\n", FILE_APPEND);
 
+$data = json_decode(file_get_contents('php://input'));
+
+$db = new mysqli('localhost', 'ben', 'digger078', 'ideascape');
+
+$if($db->connect_errno > 0) {
+    die('Unable to connect to database [' . $db->connect_error . ']');
+}
+
+if (array_key_exists("query", $data)) {
+    $url = strstr($data["query"]->url, "#", true);
+    $query = "";
+    $id = uniqid("", true);
+    if ($data["query"]->pid > 0) {
+        $query = "SELECT id FROM nodes WHERE session = '".$data["query"]->session."' AND url = '".$url."' AND pid = '"
+            .$data["query"]->pid."'";
+        if(!$result = $db->query($query)){
+            die('There was an error running the query for finding similar ID with same PID [' . $db->error . ']');
+        }
+        if ($result->num_rows == 0) {
+            $query = "INSERT INTO nodes (id, pid, url, session, token, time)"
+                ." VALUES ('"
+                    ."{$id}"
+                    ."', '{$data["query"]->pid}"
+                    ."', '{$data["query"]->url}"
+                    ."', '{$data["query"]->session}"
+                    ."', '{$data["query"]->token}"
+                    ."', 0";
+            if(!$result = $db->query($query)){
+                die('There was an error running the query adding a new entry [' . $db->error . ']');
+            }
+        } else {
+            $id = $result->fetch_assoc()["id"];
+        }
+    } else {
+        $query = "SELECT id, pid FROM nodes WHERE session = '".$data["query"]->session."' AND url = '".$url."'";
+        if(!$result = $db->query($query)){
+            die('There was an error running the query retrieving nodes with similar URL\'s [' . $db->error . ']');
+        }
+    }
+
+
+
+
+        if(!$result = $db->query($query)){
+            die('There was an error running the query adding a new entry or retrieving ID [' . $db->error . ']');
+        }
+    }
+    $query = 'SELECT id, pid FROM nodes WHERE session = \'' . $data["query"]->session . '\' AND url = \'' . $url . '\' AND '
+}
 
 ?>
